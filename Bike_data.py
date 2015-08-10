7f48bcf9a6e6784e37dda2c0ece2f97c241aa9d7
jordanfreedman <jordanefreedman@gmail.com>
2015-08-10 13:28:44 +0100

    files changed to produce data for NYC data visualisation of aactivity per 30 mins

--- Bike_data.py
+++ Bike_data.py
@@ -7,32 +7,43 @@ import collections
 
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
-cur.execute('DELETE FROM available_bikes')
+#cur.execute('DELETE FROM available_bikes')
+old_values = {}
+first_row = True
+total = 1520
 
 # insert values into moving table:
-for i in range(60):
+for i in range(total):
 
 	r = requests.get('http://www.citibikenyc.com/stations/json')
-	exec_time = parse(r.json()['executionTime'])
+
+	try: exec_time = parse(r.json()['executionTime'])
+	except: pass
 
 	cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
 	con.commit()
 
-	id_bikes = collections.defaultdict(int)
-
+	diff_bikes = collections.defaultdict(int)
+	
 	for station in r.json()['stationBeanList']:
-		id_bikes[station['id']] = station['availableBikes']
+		if first_row == False:
+			diff_bikes[station['id']] = abs((station['availableBikes'] - old_values[station['id']]))
+		else: diff_bikes[station['id']] = 0
 
-	for k,v in id_bikes.iteritems():
+		old_values[station['id']] = station['availableBikes']	
+
+	for k,v in diff_bikes.iteritems():
 		try: cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
 		except:
 			print "No column for " + str(k)
 	
 	con.commit()
 	time.sleep(60)
-	print "cycle " + str(i+1) + " complete"
+	print str(i*100/total) + "% complete"
+	first_row = False
 
 con.close()
+
 
 
 
--- analysis.py
+++ analysis.py
@@ -2,40 +2,93 @@ import pandas as pd
 import sqlite3 as lite
 import collections
 import datetime
+import time
+import numpy as np 
 
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
 
 # read into pandas dataframe
-df = pd.read_sql_query("SELECT * FROM available_bikes ORDER BY execution_time", con, index_col = 'execution_time')
+cur.execute("SELECT * FROM available_bikes")
 
-#create dictionary with keys - station ids and values - sum of changes
-hour_change = collections.defaultdict(int)
-for i,value in enumerate(df.columns):
-	column = df.iloc[:,i]
-	difference_tot = 0
-	for i, v in enumerate(column):
-		try: 
-			difference = column.iloc[i+1] - column.iloc[i]
-			difference_tot += abs(difference)
-		except: pass
-	hour_change[str(value[1:])] = difference_tot
+rows = cur.fetchall()
+cols = [desc[0] for desc in cur.description]
+bike_data = pd.DataFrame(rows, columns=cols)
+
+cur.execute("SELECT id, longitude,latitude FROM citibike_reference")
+
+rows = cur.fetchall()
+cols = [desc[0] for desc in cur.description]
+positions = pd.DataFrame(rows, columns=cols)
+
+id_col = bike_data.columns.values[1:]
+
+cur.execute('DROP TABLE torque')
+
+cur.execute("CREATE TABLE torque (id INT, difference INT, execution_time INT);")
+
+for hour in range(24):
+	for q in [0, 30]:
+			
+		beginning = int((datetime.datetime(2015,8,9,hour,q,0)).strftime('%s'))
+		end = int((datetime.datetime(2015,8,9,hour,(q+29), 59)).strftime('%s'))
+		activity = bike_data[(bike_data['execution_time'] > beginning) & (bike_data['execution_time'] <= end)]
+				
+		for i in id_col:
+				
+			if (sum(activity[i]) > 0) & (sum(activity[i]) <= 5):
+				time = datetime.datetime.fromtimestamp(beginning).strftime('%Y-%m-%d %H:%M:%S')
+				cur.execute('INSERT INTO torque (id, difference, execution_time) VALUES (?,?,?)', (i[1:], ' 1-6', time))
+			elif (sum(activity[i]) > 5) & (sum(activity[i]) <= 12):
+				time = datetime.datetime.fromtimestamp(beginning).strftime('%Y-%m-%d %H:%M:%S')
+				cur.execute('INSERT INTO torque (id, difference, execution_time) VALUES (?,?,?)', (i[1:], ' 7-14', time))
+			elif (sum(activity[i]) > 12) & (sum(activity[i]) <= 19):
+				time = datetime.datetime.fromtimestamp(beginning).strftime('%Y-%m-%d %H:%M:%S')
+				cur.execute('INSERT INTO torque (id, difference, execution_time) VALUES (?,?,?)', (i[1:], '15-24', time))
+			elif (sum(activity[i]) > 19) & (sum(activity[i]) <= 29):
+				time = datetime.datetime.fromtimestamp(beginning).strftime('%Y-%m-%d %H:%M:%S')
+				cur.execute('INSERT INTO torque (id, difference, execution_time) VALUES (?,?,?)', (i[1:], '25-39', time))
+			elif (sum(activity[i]) > 30):
+				time = datetime.datetime.fromtimestamp(beginning).strftime('%Y-%m-%d %H:%M:%S')
+				cur.execute('INSERT INTO torque (id, difference, execution_time) VALUES (?,?,?)', (i[1:], '40+', time))
+
+con.commit()
+
+cur.execute("SELECT * FROM torque")
+
+rows = cur.fetchall()
+cols = [desc[0] for desc in cur.description]
+data = pd.DataFrame(rows, columns=cols)
 
 
-# find station with highest sum
-def key_with_max(d):
-	v = list(d.values())
-	k = list(d.keys())
-	return k[v.index(max(v))]
 
-max_station = key_with_max(hour_change)
-
-# print most active station
-cur.execute("SELECT id, stationname, latitude, longitude FROM citibike_reference WHERE id = ?", (max_station,))
-data = cur.fetchone()
-print "The most active station is station id %s at %s latitude: %s longitude: %s " % data
-print "with " + str(hour_change[max_station]) + " bicycles coming and going in the hour between " + datetime.datetime.fromtimestamp(int(df.index[0])).strftime('%Y-%m-%dT%H:%M:%S') + " and " + datetime.datetime.fromtimestamp(int(df.index[-1])).strftime('%Y-%m-%dT%H:%M:%S')
-
-con.close()
+result = pd.merge(data, positions, how='left', on='id')
 
 
+
+print result
+
+result.to_csv('bicycle_data.csv', sep='\t')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+		
+	
